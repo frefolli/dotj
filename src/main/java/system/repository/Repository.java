56ff1242b7099c54d408248a96fe94229eeaa3c8
frefolli <f.refolli@campus.unimib.it>
2022-12-07 13:19:45 +0100

    system.repository.Repository bare implementation

--- src/main/java/system/repository/Repository.java
+++ src/main/java/system/repository/Repository.java
@@ -3,30 +3,32 @@ package system.repository;
 import java.util.List;
 import system.packaging.Package;
 
+/* This class acts like a Proxy
+ * */
 public class Repository {
 	LocalRepository localRepository = null;
 	RemoteRepository remoteRepository = null;
 	
 	public Repository(LocalRepository localRepository, RemoteRepository remoteRepository) {
-		// TODO
+		this.localRepository = localRepository;
+		this.remoteRepository = remoteRepository;
 	}
 	
 	public List<Package> searchPackages(String name) {
-		// TODO
-		return null;
+		return this.remoteRepository.searchPackages(name);
 	}
 	
 	public List<Package> searchPackagesLocally(String name) {
-		// TODO
-		return null;
+		return this.localRepository.searchPackages(name);
 	}
 	
 	public Package getPackage(String name) {
-		// TODO
-		return null;
+		Package package_ = null;
+		package_ = this.localRepository.getPackage(name);
+		return package_;
 	}
 	
 	public void cleanCache() {
-		// TODO
+		this.localRepository.cleanCache();
 	}
 }
